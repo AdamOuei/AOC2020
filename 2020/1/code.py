4b78f4614af64069c50f4cd334d603ebffce1d55
Omar Oueidat <oueidat@student.chalmers.se>
2020-12-01 17:24:10 +0100

    fix code

--- 2020/1/code.py
+++ 2020/1/code.py
@@ -7,16 +7,25 @@ with open((__file__.rstrip("code.py")+"input.txt"), 'rt') as input_file:
     
 input_as_int = [int(line) for line in input.splitlines()]
 sorted_list = sorted(input_as_int)
-print(sorted_list)
+result_2 = 0
 size = len(sorted_list)
-for i in input_as_int:
+i = 0
+j = size-1
+result = 0
+
+for i in input_as_int[:-1]:
+    if (2020-i) in input_as_int:
+        result = i * (2020-i)
+        break
+
+for i in input_as_int[:-1]:
     for j in input_as_int:
-        for k in input_as_int:
-            if(i + j + k == 2020):
-                print(i*j*k)
+        if (2020-i-j) in input_as_int:
+            result_2 = i * j * (2020-i-j)
+    
 
 
-print("Part One : "+ str(None))
+print("Part One : "+ str(result))
 
 
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(result_2))

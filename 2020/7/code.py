514a12e514ae9313ff858a2f2139ad67ef26ac3c
Omar Oueidat <oueidat@student.chalmers.se>
2020-12-08 15:13:25 +0100

    day 8 done

--- 2020/7/code.py
+++ 2020/7/code.py
@@ -1,14 +1,43 @@
 # Advent of code Year 2020 Day 7 solution
 # Author = Omar Oueidat
 # Date = December 2020
+import re
+import collections
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
+bags = [bag for bag in input.splitlines()]
+set_dict = collections.defaultdict(set)
+isInList = collections.defaultdict(list)
+for lines in bags:
+    color = re.match(r'(.+?) bags contain',lines)[1]
+    for amount, child in re.findall(r'(\d+) (.+?) bags?[,.]',lines):
+        amount = int(amount)
+        set_dict[child].add(color)
+        isInList[color].append((amount,child))
 
 
-print("Part One : "+ str(None))
+sets = set()
+def recursion(check_color):
+    for color in set_dict[check_color]:
+        sets.add(color)
+        recursion(color)
+
+recursion('shiny gold')
+print(sets)
+
+def recursion2 (check_color):
+    val = 0
+    for amount, child in isInList[check_color]:
+        val += amount
+        val += amount * recursion2(child)
+    return val
 
 
 
-print("Part Two : "+ str(None))
+print("Part One : "+ str(len(sets)))
+
+
+
+print("Part Two : "+ str(recursion2('shiny gold')))
---
+++ 2020/8/code.py
@@ -0,0 +1,79 @@
+# Advent of code Year 2020 Day 8 solution
+# Author = Omar Oueidat
+# Date = December 2020
+import re
+import itertools
+with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+    input = input_file.read()
+
+input_ = [line for line in input.splitlines()]
+test_input = ['nop +0',
+'acc +1',
+'jmp +4',
+'acc +3',
+'jmp -3',
+'acc -99',
+'acc +1',
+'jmp -4',
+'acc +6']
+
+actions = []
+for action in input_:
+    val = re.search(r'[+,-]\d+$',action)[0]
+    inst = re.match(r'^[a-z]{3}',action)[0]
+    actions.append((inst,val))
+
+accu = 0
+done_inst = set()
+def loop(list,index):
+    global accu
+    if index in done_inst:
+        return 
+    size = len(actions)
+    done_inst.add(index)
+    (inst,val) = list[index]
+    if inst == 'jmp':
+        index = (index + int(val)) % size
+    if inst == 'acc':
+        accu += int(val)
+        index = (index + 1) % size     
+    if inst == 'nop':
+        index = (index + 1) % size
+        
+    loop(actions,index)
+
+loop(actions, 0)
+print("Part One : "+ str(accu))
+
+def part2(n_runs):
+    pointer = 0
+    accu2 = 0
+    checked = set()
+    while  (0 <= pointer < len(actions)):
+        (inst,val) = actions[pointer]
+        if pointer == n_runs and inst == 'jmp':
+            inst = 'nop'            
+        elif pointer == n_runs and inst == 'nop':
+            inst = 'jmp'
+        if pointer in checked:
+            return None
+        checked.add(pointer)
+        if inst == 'jmp':
+            pointer += int(val) 
+        elif inst == 'acc':
+            accu2 += int(val)
+            pointer += 1   
+        elif inst == 'nop':
+            pointer += 1
+
+    return accu2
+
+results = 0
+for i in range(len(actions)):
+    res = part2(i)
+    if res is not None:
+        print('entered here')
+        results = res
+        
+print("Part Two : "+ str(results))
+        
